4358d8fa44d4c27cb2c77698fd8adff6ada37ed2
ResetOTER <vgvgvvvdc@gmail.com>
2025-03-31 20:11:10 +0800

    add demo

--- UnityDemo/ReArchDemo/Assets/ReArchDemo/ArchManager.cs
+++ UnityDemo/ReArchDemo/Assets/ReArchDemo/ArchManager.cs
@@ -1,17 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using ReArch.Core;
+using ReArchDemo;
 using UnityEngine;
 
 public class ArchManager : MonoBehaviour
 {
     private World world;
     
+    private readonly QueryDescription _entitiesToMove = new QueryDescription().WithAll<Position, Velocity, GameObjectHandle>();
     
     void Start()
     {
         world = World.Create();
+        
+        for (var index = 0; index < 150; index++)
+        {
+            world.Create(
+                new Position { Vec2 = new Vector2(1.0f, 1.0f) },
+                new Velocity { Vec2 = new Vector2(1.0f, 1.0f) }
+            );
+        }
+
+    }
+
+    void Update()
+    {
+        world.Query<Position, Velocity, GameObjectHandle>(_entitiesToMove, DoUpdate);
     }
     
-    
+    void DoUpdate(ref Position pos, ref Velocity vel, ref GameObjectHandle handle)
+    {
+        pos.Vec2 += vel.Vec2;
+    }
+
+    private void OnDestroy()
+    {
+        world.Dispose();
+        world = null;
+    }
 }
---
+++ UnityDemo/ReArchDemo/Assets/ReArchDemo/Components.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace ReArchDemo
+{
+    // NOTE: Should this not be a `Vector3` to represent something in 3D space?
+// NOTE: Should this even be wrapped in a struct at all? The `Vector` itself is the representation of the point already.
+    /// <summary>
+    ///     The <see cref="Position"/> struct
+    ///     contains the position of an entity in 3D space.
+    /// </summary>
+    public struct Position
+    {
+        // TODO: Documentation?
+        public Vector2 Vec2;
+    }
+
+// NOTE: Should this not be a `Vector3` to represent something in 3D space?
+// NOTE: Should this even be wrapped in a struct at all? The `Vector` itself is the representation of the point already.
+    /// <summary>
+    ///     The <see cref="Velocity"/> struct
+    ///     contains the velocity of an entity in 3D space.
+    /// </summary>
+    public struct Velocity
+    {
+        // TODO: Documentation?
+        public Vector2 Vec2;
+    }
+
+    public struct GameObjectHandle
+    {
+        public int RendererId;
+    }
+
+    /// <summary>
+    ///     The <see cref="Sprite"/> struct
+    ///     contains information about an entity's appearance.
+    /// </summary>
+    public struct Sprite
+    {
+        // TODO: Documentation?
+        public Texture2D Texture2D;
+        public Color Color;
+    }
+
+}
